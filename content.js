55fdc9d3b5121807019a00b5201a7080ebf8e5ad
tango025 <gaurav2550.gg@gmail.com>
2019-05-09 20:17:44 +0530

    revised logic

--- content.js
+++ content.js
@@ -2,15 +2,46 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         var vmcArray = $("._3tz_._7794 ._4sxc._42ft");
-            count = 0;
-            while(count<4 && vmcArray.length != 0){
-            for (var i = 0; i < vmcArray.length; i++)
-            {
-                vmcArray[i].click();
-                count++;
-            }
-                vmcArray = $("._3tz_._7794 ._4sxc._42ft");
-            };
+        var arr2 = $("._6w1q._77bi ._4sxc._42ft");
+       
+        executeArray(vmcArray,onceFinished);
+
+            
+    
+        
+        
+        
+            
             sendResponse({ proto: "protocol" });
     });
 
+
+//          while (count < 4 && vmcArray.length != 0) {
+            // for (var i = 0; i < vmcArray.length; i++)
+            // {
+            //     vmcArray[i].click();
+            //     count++;
+            // }
+            //     vmcArray = $("._3tz_._7794 ._4sxc._42ft");
+            // };
+
+function executeArray(vmcArray,callback) {
+    for (var i = 0; i < vmcArray.length; i++) {
+        if (vmcArray.hasOwnProperty(i)) {
+            (function (capI) {
+                setTimeout(() => {
+                    vmcArray[capI].click();
+                }, 1000);
+            })(i);
+        }
+
+    };
+    callback();
+}
+function onceFinished(){
+    var arr = $("._3tz_._7794 ._4sxc._42ft");
+    if(arr.length){
+        executeArray(arr,onceFinished);
+        return undefined;
+    }
+}
